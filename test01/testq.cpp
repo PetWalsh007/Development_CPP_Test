ad8d0330c500d772bd13c9f9149493da21fb50ce
Peter Walsh <peterwalsh0701@gmail.com>
2025-06-03 16:31:18 +0100

    Test setup

---
+++ test01/testq.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+// Function prototype
+void newFunction();
+
+int main() {
+
+    vector<int> numbers = {5, 3, 8, 6, 2};
+    sort(numbers.begin(), numbers.end());
+    
+    cout << "Sorted numbers: ";
+    for (const auto& num : numbers) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    // Call a function
+    newFunction();
+
+    return 0;
+}
+
+void newFunction() {
+    cout << "This is a new function" << endl;
+}
+
+
+
